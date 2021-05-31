f075856e23907a650a5b4f0c3a4368c9da386b76
samane <s_hajizade63@yahoo.com>
2021-05-31 21:33:59 +0200

    add schema for books in graphql

--- app.js
+++ app.js
@@ -1,35 +1,33 @@
 const express = require('express');
 var { graphqlHTTP } = require('express-graphql');
 var { buildSchema } = require('graphql');
+const schema = require('./schema/schema');
+
 http = require('http');
 
 const hostname = 'localhost';
 const port = 4000;
 
 // Construct a schema, using GraphQL schema language
-var schema = buildSchema(`
-  type Query {
-    hello: String
-  }
-`);
+// var schema = buildSchema(`
+//   type Query {
+//     hello: String
+//   }
+// `);
 
 // The root provides a resolver function for each API endpoint
-var root = {
-  hello: () => {
-    return 'Hello world!';
-  },
-};
+// var root = {
+//   hello: () => {
+//     return 'Hello world!';
+//   },
+// };
 
 const app = express();
 
 // bind express with graphql
-app.use('/graphql', graphqlHTTP({
-    // pass in a schema property
-    schema: schema,
-    rootValue: root,
-    graphiql: true,
-})
-);
+app.use('/graphql', graphqlHTTP({  
+schema
+}));
 
 
   app.listen(port, hostname, () => {
