87aef883150824a4346f4a99d8fff63ce581d9a4
samane <s_hajizade63@yahoo.com>
2021-05-31 23:15:53 +0200

    Find books/authors queries

--- readme.txt
+++ readme.txt
@@ -1,3 +1,6 @@
+*** author with lists of books***
+
+
 1.mkdir NodeJS & go inside
 2.npm init
 3.Create index.js file
@@ -14,10 +17,12 @@ npm install lodash
 http://localhost:4000/graphql in url in graphiql:
 Find book:
 {
+  book(id:1){
     id,
     genre,
     name
   }
+}
 
 Find author:
 {
@@ -37,5 +42,27 @@ Find book and belongs'author:
     name
     age
   }
+ }
+}
+
+Find author with his books':
+{
+  author(id: 1) {
+    id
+    name
+    books{
+    name
   }
+ }
+}
+
+Find all books/authors :
+{
+  authors {
+    id
+    name
+    books{
+    name
+  }
+ }
 }
--- schema/schema.js
+++ schema/schema.js
@@ -2,18 +2,22 @@ const graphql = require('graphql');
 const _ = require('lodash');
 
 const {
-    GraphQLObjectType, 
-    GraphQLString, 
+    GraphQLObjectType,
+    GraphQLString,
     GraphQLSchema,
     GraphQLID,
-    GraphQLInt
- } = graphql;
+    GraphQLInt,
+    GraphQLList
+} = graphql;
 
 // dummy data
 var books = [
-    { name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId:'1' },
-    { name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId:'2'},
-    { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' ,authorId:'3'},
+    { name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1' },
+    { name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2' },
+    { name: 'The Hero of Ages', genre: 'Fantasy', id: '4', authorId: '2' },
+    { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3' },
+    { name: 'The Colour of Magic', genre: 'Fantasy', id: '5', authorId: '3' },
+    { name: 'The Light Fantastic', genre: 'Fantasy', id: '6', authorId: '3' },
 ];
 var authors = [
     { name: 'Patrick Rothfuss', age: 44, id: '1' },
@@ -27,13 +31,14 @@ const BookType = new GraphQLObjectType({
         id: { type: GraphQLID },
         name: { type: GraphQLString },
         genre: { type: GraphQLString },
-        author: { 
+        author: {
             type: AuthorType,
-            resolve(parent, args){
+            resolve(parent, args) {
+                console.log(parent);
                 return _.find(authors, { id: parent.authorId });
 
             }
-         }
+        }
     })
 });
 
@@ -42,7 +47,15 @@ const AuthorType = new GraphQLObjectType({
     fields: () => ({
         id: { type: GraphQLID },
         name: { type: GraphQLString },
-        age: { type: GraphQLInt}
+        age: { type: GraphQLInt },
+        books: {
+            type: new GraphQLList(BookType),
+            resolve(parent, args) {
+                // console.log(parent);
+                return _.filter(books, { authorId: parent.id });
+
+            }
+        }
     })
 });
 
@@ -52,7 +65,7 @@ const RootQuery = new GraphQLObjectType({
         book: {
             type: BookType,
             args: { id: { type: GraphQLID } },
-            resolve(parent, args){
+            resolve(parent, args) {
                 // code to get data from db / other source
                 return _.find(books, { id: args.id });
 
@@ -61,21 +74,24 @@ const RootQuery = new GraphQLObjectType({
         author: {
             type: AuthorType,
             args: { id: { type: GraphQLID } },
-            resolve(parent, args){
+            resolve(parent, args) {
                 // code to get data from db / other source
                 return _.find(authors, { id: args.id });
 
             }
         },
         books: {
-            type: BookType,
-            args: { id: { type: GraphQLID } },
-            resolve(parent, args){
-                // code to get data from db / other source
-                return _.find(books);
-
+            type: new GraphQLList(BookType),
+            resolve(parent, args) {
+                return books;
             }
         },
+        authors: {
+            type: new GraphQLList(AuthorType),
+            resolve(parent, args) {
+                return authors;
+            }
+        }
     }
 });
 
