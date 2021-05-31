71e7cce2bf0e96f3e618e19896b7898ec3124f2d
samane <s_hajizade63@yahoo.com>
2021-05-31 21:57:38 +0200

    change bookid to GRAPHQLID works for both string and ID

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+const { trace } = require('console');
 const express = require('express');
 var { graphqlHTTP } = require('express-graphql');
+const { renderGraphiQL } = require('express-graphql/renderGraphiQL');
 var { buildSchema } = require('graphql');
 const schema = require('./schema/schema');
 
@@ -8,27 +10,13 @@ http = require('http');
 const hostname = 'localhost';
 const port = 4000;
 
-// Construct a schema, using GraphQL schema language
-// var schema = buildSchema(`
-//   type Query {
-//     hello: String
-//   }
-// `);
-
-// The root provides a resolver function for each API endpoint
-// var root = {
-//   hello: () => {
-//     return 'Hello world!';
-//   },
-// };
-
 const app = express();
 
 // bind express with graphql
 app.use('/graphql', graphqlHTTP({  
-schema
+schema,
+graphiql : true
 }));
-
 
   app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
--- readme.txt
+++ readme.txt
@@ -9,3 +9,11 @@ npm install express --save
 npm install --save express-graphql
 check http://localhost:4000/graphql:
 This screen shot shows the GraphQL query { hello } being issued and giving a result of { data: { hello: 'Hello world!' } }
+
+npm install lodash
+http://localhost:4000/graphql in url in graphiql:
+{
+    id,
+    genre,
+    name
+  }
