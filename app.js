1c7ca5d22869ea77ce0a33d63a4ea20d80394d16
samane <s_hajizade63@yahoo.com>
2021-06-02 00:35:35 +0200

    Fix bug/ using mongo client to connect to database. Try to connect to mongodb database by using mongoose.connect()

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express = require('express');
 
 var { graphqlHTTP } = require('express-graphql');
-const { renderGraphiQL } = require('express-graphql/renderGraphiQL');
-var { buildSchema } = require('graphql');
+
 const schema = require('./schema/schema');
 
-const assert = require('assert');
+const mongoose = require('mongoose');
 
 var mongoDb = require('mongodb');
 var mongoClient = mongoDb.MongoClient;
 var serverUrl = "mongodb://127.0.0.1:27017/";
+// var url = "mongodb+srv://samane:840921@cluster0.zuu8h.mongodb.net/myLibrary";
+
+
+
 var dbName = "library";
 
 var http = require('http');
@@ -17,9 +20,18 @@ var http = require('http');
 const hostname = 'localhost';
 const port = 4000;
 
-mongoClient.connect(serverUrl, { useNewUrlParser: true, useUnifiedTopology: true }, function (err, db) {
-   // Code goes here...
-});
+// mongoClient.connect(serverUrl, { useNewUrlParser: true, useUnifiedTopology: true }, function (err, db) {
+//    // Code goes here...
+//    console.log('conneted to database');
+// });
+
+
+// mongoose.connect(serverUrl);
+// mongoose.connection.once('open', () => {
+//   console.log('conneted to database');
+// });
+
+mongoose.connect(serverUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
 const app = express();
 
