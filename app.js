72a696d17e85dc5a9f5fa9ed64aa70579005fcf4
samane <s_hajizade63@yahoo.com>
2021-06-02 15:24:57 +0200

    Fix bug for connection beween DB and GraphQL

--- app.js
+++ app.js
@@ -1,39 +1,28 @@
 const express = require('express');
-
 var { graphqlHTTP } = require('express-graphql');
-
 const schema = require('./schema/schema');
-
 const mongoose = require('mongoose');
 
-var mongoDb = require('mongodb');
-var mongoClient = mongoDb.MongoClient;
+const app = express();
+
 var serverUrl = "mongodb://127.0.0.1:27017/";
-// var url = "mongodb+srv://samane:840921@cluster0.zuu8h.mongodb.net/myLibrary";
-
-
-
 var dbName = "library";
-
-var http = require('http');
 
 const hostname = 'localhost';
 const port = 4000;
 
-// mongoClient.connect(serverUrl, { useNewUrlParser: true, useUnifiedTopology: true }, function (err, db) {
-//    // Code goes here...
-//    console.log('conneted to database');
-// });
+// connect to mlab database
+mongoose.connect('mongodb://127.0.0.1:27017/library');
+mongoose.connection.once('open', () => {
+  console.log('conneted to database');
+});
 
+// mongoose.connect(serverUrl,
+//   { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true },
+//   function (err, dbName) {
+//     console.log(dbName.dbName + ' db conneted to database');
+//   });
 
-// mongoose.connect(serverUrl);
-// mongoose.connection.once('open', () => {
-//   console.log('conneted to database');
-// });
-
-mongoose.connect(serverUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-
-const app = express();
 
 
 // bind express with graphql
--- schema/schema.js
+++ schema/schema.js
@@ -12,20 +12,6 @@ const {
     GraphQLList
 } = graphql;
 
-// var books = [
-//     { name: 'Name of the Wind', genre: 'Fantasy', id: '1' },
-//     { name: 'The Final Empire', genre: 'Fantasy', id: '2' },
-//     { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' },
-//     { name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId:'1' },
-//     { name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId:'2'},
-//     { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' ,authorId:'3'},
-// ];
-// var authors = [
-//     { name: 'Patrick Rothfuss', age: 44, id: '1' },
-//     { name: 'Brandon Sanderson', age: 42, id: '2' },
-//     { name: 'Terry Pratchett', age: 66, id: '3' }
-// ];
-
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({
