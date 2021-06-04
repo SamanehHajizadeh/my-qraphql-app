fdae82f6833fd2eb03855d8f73cbb9632ae7ca8d
samane <s_hajizade63@yahoo.com>
2021-06-04 17:09:35 +0200

    Update DB connection

--- server/app.js
+++ server/app.js
@@ -2,26 +2,23 @@ const express = require('express');
 var { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 
-var serverUrl = "mongodb://127.0.0.1:27017/";
+app.use(cors());
+
+var url = "mongodb://127.0.0.1:27017/";
 var dbName = "library";
+var serverUrl = 'mongodb://127.0.0.1:27017/library';
 
 const hostname = 'localhost';
 const port = 4000;
 
-// connect to mlab database
-mongoose.connect('mongodb://127.0.0.1:27017/library');
-mongoose.connection.once('open', () => {
-  console.log('conneted to database');
-});
-
-// mongoose.connect(serverUrl,
-//   { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true },
-//   function (err, dbName) {
-//     console.log(dbName.dbName + ' db conneted to database');
-//   });
+mongoose.connect(serverUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true },
+  function (err, dbName) {
+    console.log(serverUrl + ' db conneted to database');
+  });
 
 
 
